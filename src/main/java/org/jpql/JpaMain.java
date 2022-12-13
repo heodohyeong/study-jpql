dcddb95bf38ebd61ca59d3c2ce11c03bee8261f6
hdh <jackt5000@yooncoms.com>
2022-12-13 16:04:55 +0900

    study-jpql

---
+++ src/main/java/org/jpql/JpaMain.java
@@ -0,0 +1,43 @@
+package org.jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+     public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member singleResult1 = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println(singleResult1.getUsername());
+
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+
+
+    }
+
+
+}
